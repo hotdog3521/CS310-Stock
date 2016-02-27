85d3ccae3a1352aabf917b51bdcda51210814140
Brandon Cen <bcen96@gmail.com>
2016-02-26 16:16:04 -0800

    basic User class implemented)

--- php_classes/user.php
+++ php_classes/user.php
@@ -1,5 +1,24 @@
 <?php
 class User{
 
+	private $username;
+	private $password;
+	private $portfolio;
+
+	public function __construct($userName, $passWord){
+		$this->username = $userName;
+		$this->password = $passWord;
+	}
+	public function setPassword($passWord){
+		$this->password = $passWord;
+	}
+
+	public function getPortfolio(){
+		return $this->portfolio;
+	}
+	public function setPortfolio($Portfolio){
+		$this->portfolio = $Portfolio;
+	}
+
 }
 ?>
