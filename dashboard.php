b78c0d481751026a70fac3059f27f722dbc421f7
Patrick Vanderwall <pvanderw@usc.edu>
2016-02-27 22:51:35 +0000

    finished login

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<?php
+require_once("header.php");
+?>
+
+<div class="container">
+	<div class="col-md-6 well" style="margin:20px auto; float:none;">
+		<h1>Dashboard</h1>
+		
+	</div>
+</div>
+
+
+</body>
+</html>
