9cf78040d22ee693a02f98452a174998b0a87d24
Cyrus Jia <cjia@usc.edu>
2016-02-28 18:02:11 -0800

    integrated google finance data + get stock information into APIManager

--- API/client.php
+++ API/client.php
@@ -3,9 +3,13 @@
 
 class client{
 
+  public function __construct(){
+
+
+  }
 
   public function getStock($Symbol){
-    $data = file_get_contents('http://finance.google.com/finance/info?client=ig&q=' . $Symbol);
+    $data = file_get_contents('http://finance.google.com/finance/info?q=' . $Symbol);
     return $data;
   }
 
