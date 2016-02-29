f7b20c5bc6f23f0d07e41006e88f019541caf42a
Cyrus Jia <cjia@usc.edu>
2016-02-28 18:35:10 -0800

    apimanager/client classes fixed getstock

--- API/client.php
+++ API/client.php
@@ -9,8 +9,20 @@ class client{
   }
 
   public function getStock($Symbol){
-    $data = file_get_contents('http://finance.google.com/finance/info?q=' . $Symbol);
-    return $data;
+    if($data = @file_get_contents('http://finance.google.com/finance/info?q=' . $Symbol)){
+      $data = str_replace("\n", "", $data);
+      $data = substr($data, 4, strlen($data) -5);
+      $json = json_decode(utf8_decode($data));
+
+      $output = array(
+          "symbol" => $json->t,
+          "market" => $json->e,
+          "price" => $json->l,
+          "datetime" => $json->lt
+      );
+
+      return $output;
+    }
   }
 
 }
