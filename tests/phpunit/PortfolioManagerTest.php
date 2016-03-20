6087fbb31440461161d72d4fad3c38c1939a0d89
Brandon Cen <bcen@usc.edu>
2016-03-20 18:57:37 +0000

    bug fix id in PMtest

--- tests/phpunit/PortfolioManagerTest.php
+++ tests/phpunit/PortfolioManagerTest.php
@@ -7,70 +7,100 @@
 		// testing functions using $balance
 
 		public function testGetZeroBalance(){
-			$pm = new PortfolioManager(0);
-			$pm->loadNewPortfolio(NULL, 0, 0, NULL);
+			// Arrange
+			$pm = new PortfolioManager(14);
+			$pm->loadNewPortfolio(NULL,0, 0, NULL);
+			// Act
+			// Assert
 			$this->assertEquals(0, $pm->getBalance());
 		}
 
 		public function testGetPositiveBalance(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 3, 0, NULL);
+			// Act
+			// Assert
 			$this->assertEquals(3, $pm->getBalance());
 		}
 
 		public function testSetNegativeBalance(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 0, 0, NULL);
+			// Act
 			$pm->setBalance(-1);
+			// Assert
 			$this->assertEquals(0, $pm->getBalance());
 		}
 
 		public function testSetZeroBalance(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 3, 0, NULL);
+			// Act
 			$pm->setBalance(0);
+			// Assert
 			$this->assertEquals(0, $pm->getBalance());
 		}
 
 		public function testSetPositiveBalance(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 0, 0, NULL);
+			// Act
 			$pm->setBalance(5);
+			// Assert
 			$this->assertEquals(5, $pm->getBalance());
 		}
 
 		// testing functions using $netPortfolioValue
 
 		public function testGetZeroNetPortfolioValue(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 0, 0, NULL);
+			// Act
+			// Assert
 			$this->assertEquals(0, $pm->getNetPortfolioValue());
 		}
 
 		public function testGetPositiveNetPortfolioValue(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 0, 5, NULL);
+			// Act
+			// Assert
 			$this->assertEquals(5, $pm->getNetPortfolioValue());
 		}
 
 		public function testSetNegativeNetPortfolioValue(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 0, 0, NULL);
+			// Act
 			$pm->setBalance(-1);
+			// Assert
 			$this->assertEquals(0, $pm->getNetPortfolioValue());
 		}
 
 		public function testSetZeroNetPortfolioValue(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 0, 3, NULL);
+			// Act
 			$pm->setBalance(0);
+			// Assert
 			$this->assertEquals(0, $pm->getNetPortfolioValue());
 		}
 
 		public function testSetPositiveNetPortfolioValue(){
-			$pm = new PortfolioManager(0);
+			// Arrange
+			$pm = new PortfolioManager(14);
 			$pm->loadNewPortfolio(NULL, 0, 0, NULL);
+			// Act
 			$pm->setBalance(5);
+			// Assert
 			$this->assertEquals(5, $pm->getNetPortfolioValue());
 		}
 	}
